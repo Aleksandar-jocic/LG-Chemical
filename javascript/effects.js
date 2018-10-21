99692c2ae9e3a72cb0ecf79835c8198f553478b9
aleksandar-jocic <aleksandar.7jocic@gmail.com>
2018-10-21 18:46:07 +0200

    effects.js fix

---
+++ javascript/effects.js
@@ -0,0 +1,67 @@
+var navbar = $('.overlay');
+var logoText = $('.logoText');
+var timeOne;
+var timeTwo;
+var handler;
+var textAppear;
+
+$(document).ready(function () {
+
+    timeOne = setTimeout(handler, 1000);  
+    timeTwo = setTimeout(textAppear, 1500);
+})
+
+
+handler = function() {
+    navbar.addClass('overlay-active')
+    console.log('handle works');
+    clearTimeout()
+}
+$('.carousel').carousel({
+    interval: 55000
+})
+
+textAppear = function() {
+    logoText.addClass('logoText-active');
+    console.log('offset works');
+    clearTimeout()
+    
+}
+
+
+
+window.onscroll = function() {fixNavbar()};
+
+
+function fixNavbar() {
+    
+    if (window.pageYOffset > 0) {
+
+      navbar.addClass("sticky");
+      
+    } else {
+
+      navbar.removeClass("sticky");
+
+    }    
+}
+
+document.getElementById('target').addEventListener('click', function () {
+
+    if ($('.options').hasClass('options-responsive')) {
+        $('.options').removeClass('options-responsive');
+    }
+    else {
+        $('.options').addClass('options-responsive');
+    }
+})
+
+document.getElementById('productTrigger').addEventListener('click', function () {
+
+    if ($('.catalogue').hasClass('show')) {
+        $('.catalogue').removeClass('show');
+    }
+    else {
+        $('.catalogue').addClass('show');
+    }
+})
