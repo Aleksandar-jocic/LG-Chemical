c5499e679ce345e1eb5367aac3c2bdbbf02692dc
aleksandar-jocic <aleksandar.7jocic@gmail.com>
2018-12-07 17:00:35 +0100

    refreshing DB and santizing mail

--- administration/contact.php
+++ administration/contact.php
@@ -1,21 +1,32 @@
+<?php include "../productDB/serverConnection.php"; ?>
+
 <?php 
 if(isset($_POST['submit'])){
 
-        // this is your Email address
-    $to = "aleksandar.7jocic@gmail.com"; 
-        // this is the sender's Email address
-    $from = $_POST['email']; 
+    global $connection;
 
-    $first_name = $_POST['first_name'];
+    $to = "aleksandar.7jocic@gmail.com"; 
+
+    $client_email = $_POST['email']; 
+    $client_email = filter_var($client_email, FILTER_SANITIZE_EMAIL);
+
+    $client_name = $_POST['first_name'];
+    $client_name = filter_var($client_name, FILTER_SANITIZE_STRING);
+    // $client_name = mysqli_real_escape_string($connection, $client_name);
+
+    $message_content = $_POST['message'];
+    $message_content = filter_var($message_content, FILTER_SANITIZE_STRING);
+    // $message_content = mysqli_real_escape_string($connection, $message_content);
+
     $subject = "Form submission";
 
-    $message = $first_name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message = $client_name . " " . " wrote the following:" . "\n\n" . $message_content;
 
-    $headers = "From:" . $from;
+    $headers = "From:" . $client_email;
     
     mail($to,$subject,$message,$headers);
 
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    echo "Mail Sent. Thank you " . $client_name . ", we will contact you shortly. " . $client_email . "<br>" . $message_content;
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
 ?>
