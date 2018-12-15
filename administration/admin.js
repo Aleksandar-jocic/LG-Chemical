708db7dc16fc7a43cb4383b672dd55d4ee49731b
aleksandar-jocic <aleksandar.7jocic@gmail.com>
2018-12-15 16:49:29 +0100

    adding basic admin sec

---
+++ administration/admin.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+  
+    var changeId;
+    $('.changeMe').click(function () {
+      var el = this;
+      var id = this.id;
+      var splitid = id.split("_");
+      changeId = splitid[1];
+      $('.update').css('display', 'block');
+    });
+
+        $('.changePerson').click(function () {
+
+        console.log(changeId);
+          
+
+          var newNameFor = $('.newNameInput').val();
+          var newLastNameFor = $('.newLastNameInput').val();
+
+          var data = {changeSth: changeId, newName: newNameFor, newLastName: newLastNameFor};
+
+          $.ajax({
+
+            url: './adminFunctions.php',
+            type: 'POST',
+            data: data,
+            success: function(response) {
+
+                if(response == 1) {
+
+                  
+                  var id = $('#cha_' + changeId);
+
+                  id.parent().fadeOut(800,function(){
+
+                    id.parent().show('');
+                    
+                      console.log(id.parent());
+                      
+
+                  });
+                  
+                  console.log("response good");
+                  
+                } else {
+
+                  console.log("response not so good");
+
+                }
+            }
+          });
+
+        });
+
+    // Delete 
+    $('.deleteMe').click(function(){
+
+      var el = this;
+      var id = this.id;
+      var splitid = id.split("_");
+
+      // Delete id
+      var deleteid = splitid[1];
+      //  console.log(deleteid);
+
+      // AJAX Request
+      $.ajax({
+        url: './adminFunctions.php',
+        type: 'POST',
+        data: 'deleteSth=' + deleteid,
+        success: function(response){
+
+          if(response == 1){
+        // Remove row from HTML Table
+        console.log("deleted");
+        
+        $(el).parent().fadeOut(800,function(){
+            $(this).remove();
+        console.log("deleted");
+
+        });
+          }else{
+            console.log(response);
+            
+            alert('Invalid ID.');
+          }
+
+        }
+      });
+
+    });
+   
+});
--- administration/admin.php
+++ administration/admin.php
@@ -1,111 +1,145 @@
-<?php
-   
-   
+<?php include "../productDB/serverConnection.php"; ?>
+<?php include "./adminFunctions.php"; ?>
 
 
-
+<?php 
+  session_start(); 
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Admin panel</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LG hemija</title>
+    <link rel="stylesheet" href="../css/preLoader.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Montserrat|Sunflower:300" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+        crossorigin="anonymous">
+
 </head>
 <body>
 
+<div class="adminHead">
+    <h2>Admin Page</h2>
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<!-- <p> <a href="admin.php?logout='1'" style="color: red;">logout</a> </p> -->
+    <?php endif ?>
+</div>
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
+
+    <!-- logged in user information -->
     
+</div>
 
-<?php 
-        $admin = false;
-        $serverName = "localhost";
-        $userName = "root";
-        $password = "";
-        $dbname = "admins";
-        $connection = mysqli_connect($serverName, $userName, $password, $dbname);
+<div class="adminMain">
 
-    if(isset($_POST['submit'])) {
+	<div class="adminMainSelect"><a href="./admin.php?ext=products">Products</a></div>
+	<div class="adminMainSelect"><a href="./admin.php?ext=news">News</a></div>
+	<div class="adminMainSelect"><a href="./admin.php?ext=partners">Partners</a></div>
 
-    $userName = $_POST["username"];
-    $password = $_POST["password"];
+</div>
 
-    $userName = mysqli_real_escape_string($connection, $userName);
-    $password = mysqli_real_escape_string($connection, $password);
+	<?php 
+	
+	if(isset($_GET['ext'])) {
+		$ext = $_GET['ext'];
+		if($ext === 'products') {
+	?>
+			<div class="productsSelected">
+				<div><a href="./admin.php?ext=products&selected=main_group">Main groups</a></div>
+				<div><a href="./admin.php?ext=products&selected=sub_group">Sub groups</a></div>
+				<div><a href="./admin.php?ext=products&selected=product">Products</a></div>
+			</div>
 
-    $query = "SELECT * FROM adminlist";
-
-    $getAdmin = mysqli_query($connection, $query);
-    
-    if(!$getAdmin) {
-
-        die("fail");
-
-    } 
-    while($row = mysqli_fetch_array($getAdmin)) {
-
-        $adminUser = $row['username'];
-        $adminPass = $row['password'];
+	<?php		
+		} else if ($ext === 'news') {
+			getNews();
+		} else if ($ext === 'partners') {
+			getPartners();
+		}
     }
-    if ($userName === $adminUser && $password === $adminPass) {
-
-        $admin = true;
-
-    } else {
-
-        echo "wrong password";
-    }
-}
-if (!$admin) {
-?>
-        <form method="POST" action="admin.php"><br>
-        Username: <input type="text" name="username"><br><br><br>
-        Password: <input type="password" name="password"><br><br>
-        <input type="submit" name="submit" value="submit">
+    ?>
+        <div class="adminOutput">
 <?php
-
-    } else {
-?>
-    <h1>LG-Chemical Administration panel</h1>
-
-    products panel
-
-        -main group
-
-            ===add
-            ===edit
-            ===remove
-
-        -sub group
-
-            ===add
-            ===edit
-            ===remove
-
-        -products group
-
-            ===add
-            ===edit
-            ===remove
-
-    news panel
-
-        -feed list
+	if(isset($_GET['selected'])) {
+        $selected = $_GET['selected'];
         
-            ==add
-            ===edit
-            ===remove
-
-    admin side panel
-
-        -change admin credentials 
-
-<?php        
-    }
+		if($selected === 'main_group') {
+			getMainProducts();
+		} else if ($selected === 'sub_group') {
+			getSubProducts();
+		} else {
+			getProducts();
+		}
+	}
 ?>
- 
-    
+        </div>
+
+	<div>
+	<?php 
+	
+	testdb();
+	
+	
+	?>
+	<form method="post" action="adminFunctions.php">
+		<div class="input-group">
+			<label>first name</label>
+			<input type="text" name="firstName" >
+		</div>
+		<div class="input-group">
+			<label>last name</label>
+			<input type="text" name="lastName">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn uploadPerson" name="uploadPerson">enter</button>
+		</div>
+  	</form>
+
+	<div class="update" style="display:none">
+		<form method="post" action="adminFunctions.php">
+			<div class="input-group">
+				<label>new name</label>
+				<input class='newNameInput' type="text" name="newName" >
+			</div>
+			<div class="input-group">
+				<label>new last name</label>
+				<input class='newLastNameInput' type="text" name="newLastName">
+			</div>
+			<div class="input-group">
+				<button type="button" class="btn changePerson" name="changePerson">change</button>
+			</div>
+		</form>
+	</div>
+	
+	</div>
+		<script src="./admin.js"></script>
 </body>
 </html>
